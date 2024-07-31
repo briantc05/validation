29b0700217d0b4ff43bf36f6f1a7812cbce1d989
Brian Tamayo Colcol <briantamayocolcol@gmail.com>
2024-07-31 10:33:30 +0800

    Validation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserTaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('user_task', UserTaskController::class);
